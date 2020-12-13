22bcde8591ee640b3eea83f8bcdf64294e017300
TrueVincentVega <drpeso.jr@gmail.com>
2020-12-13 21:15:36 +0200

    Добавьте файлы проекта.

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿
+namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.score_label = new System.Windows.Forms.Label();
+            this.scoreinfo_label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 25;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // score_label
+            // 
+            this.score_label.AutoSize = true;
+            this.score_label.BackColor = System.Drawing.SystemColors.Window;
+            this.score_label.Font = new System.Drawing.Font("Snap ITC", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.score_label.Location = new System.Drawing.Point(838, 9);
+            this.score_label.Name = "score_label";
+            this.score_label.Size = new System.Drawing.Size(30, 27);
+            this.score_label.TabIndex = 0;
+            this.score_label.Text = "0";
+            // 
+            // scoreinfo_label
+            // 
+            this.scoreinfo_label.AutoSize = true;
+            this.scoreinfo_label.BackColor = System.Drawing.SystemColors.Info;
+            this.scoreinfo_label.Font = new System.Drawing.Font("Snap ITC", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scoreinfo_label.Location = new System.Drawing.Point(745, 9);
+            this.scoreinfo_label.Name = "scoreinfo_label";
+            this.scoreinfo_label.Size = new System.Drawing.Size(87, 27);
+            this.scoreinfo_label.TabIndex = 1;
+            this.scoreinfo_label.Text = "Score:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(894, 592);
+            this.Controls.Add(this.scoreinfo_label);
+            this.Controls.Add(this.score_label);
+            this.Font = new System.Drawing.Font("Showcard Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Duck Hunt";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label score_label;
+        private System.Windows.Forms.Label scoreinfo_label;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {   
+        // змінні 
+        public Bitmap HandlerTexture = Resource1.Handler;
+        public Bitmap TargetTexture = Resource1.Target;
+        private Point _targetPosition = new Point(300, 300);
+        private Point _direction = Point.Empty; // направление
+        private int _score = 0; // поле з початковим рахунком
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Виправлення мерехтіння
+            SetStyle(ControlStyles.OptimizedDoubleBuffer |
+                ControlStyles.AllPaintingInWmPaint |
+                ControlStyles.UserPaint, true); 
+            UpdateStyles();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics graphics = e.Graphics;
+
+            var localPosition = this.PointToClient(Cursor.Position); // змінна поточної позиції курсору на екрані (переводимо в точку на формі)
+            var handleRect = new Rectangle(localPosition.X - 50, localPosition.Y - 50, 100, 100); // змінна позиції фіолетової плями відносно курсору
+            var targetRect = new Rectangle(_targetPosition.X - 50, _targetPosition.Y - 50, 100, 100);
+
+            // направлення сяйва та підвищення швидкості зміни координат
+            _targetPosition.X += _direction.X * 5;
+            _targetPosition.Y += _direction.Y * 5;
+
+            // Перевірка щоб сяйво не виходило за області видимості
+            if (_targetPosition.X < 0 || _targetPosition.X > 500 )
+            {
+                //_targetPosition.X *= -1;
+                //_targetPosition.X = 200;
+                _direction.X *= -1;
+
+            }
+            if (_targetPosition.Y < 0 || _targetPosition.Y > 500)
+            {
+                //_targetPosition.Y *= -1;
+                //_targetPosition.Y = 200;
+                _direction.Y *= -1;
+            }
+
+            Point between = new Point(localPosition.X - _targetPosition.X, localPosition.Y - _targetPosition.Y); // проміжне значення
+            float distance = (float)Math.Sqrt((between.X * between.X)+(between.Y * between.Y)); // теорема Піфагора для визначення гіпотенузи
+            
+            // перевірка на попадання
+           if(distance < 10)
+            {
+                AddScore(1);
+            }
+
+            graphics.DrawImage(HandlerTexture, handleRect); // фіолетова пляма
+            graphics.DrawImage(TargetTexture, targetRect); // сине сяйво
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Refresh(); // Перерисовка форми по таймеру
+        }
+
+        private void timer2_Tick(object sender, EventArgs e) // таймер для руху сяйва
+        {
+            Random random = new Random();
+            timer2.Interval = random.Next(25, 1000);
+            _direction.X = random.Next(-1, 2);
+            _direction.Y = random.Next(-1, 2);
+        }
+
+        private void AddScore(int score)
+        {
+            _score += score;
+            score_label.Text = _score.ToString();
+        }
+    }
+}
